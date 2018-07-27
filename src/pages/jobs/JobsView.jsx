6fad55beef4c3f67c490c1add404138545c57277
Long Tran <long.tran@axonactive.com>
2018-07-27 09:16:44 +0700

    Add pagination ui

--- src/pages/jobs/JobsView.jsx
+++ src/pages/jobs/JobsView.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Row, Col, Panel, Button,
-  ButtonToolbar, Modal, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+  ButtonToolbar, Modal, FormGroup, ControlLabel,
+  FormControl, Pagination } from 'react-bootstrap';
 import JobList from './components/JobList';
+
+import './job.css';
 
 /* eslint no-confusing-arrow: 0 */
 class JobsView extends React.PureComponent {
@@ -35,6 +38,15 @@ class JobsView extends React.PureComponent {
     });
   }
 
+  renderPaginationView = () => {
+    const active = 2;
+    const items = [];
+    for (let number = 1; number <= 10; number += 1) {
+      items.push(<Pagination.Item active={number === active}>{number}</Pagination.Item>);
+    }
+    return items;
+  }
+
   render() {
     return (
       <Grid>
@@ -42,10 +54,10 @@ class JobsView extends React.PureComponent {
           <Panel>
             <Panel.Heading>
               <Row className="show-grid">
-                <Col xs={6} md={8}>
+                <Col xs={6} md={8} className="vcenter">
                   <h3 className="panel-title">Jobs</h3>
                 </Col>
-                <Col xs={6} md={4} className="text-right">
+                <Col xs={6} md={4} className="text-right vcenter">
                   <ButtonToolbar className="pull-right">
                     <Button bsStyle="primary" className="mr-1" onClick={this.handleOpenModal}>Create a job</Button>
                     <Button bsStyle="primary">Export CSV</Button>
@@ -60,6 +72,16 @@ class JobsView extends React.PureComponent {
                 updateJob={() => {}}
               />
             </Panel.Body>
+            <Panel.Footer>
+              <Row className="show-grid">
+                <Col xs={4} md={4} className="vcenter">
+                  <div>Page 1 of 5</div>
+                </Col>
+                <Col xs={8} md={8} className="vcenter">
+                  <Pagination className="m-0 pull-right" bsSize="medium">{this.renderPaginationView()}</Pagination>
+                </Col>
+              </Row>
+            </Panel.Footer>
           </Panel>
         </Row>
         <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
